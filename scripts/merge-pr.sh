0a7bfd92039f2fa065b01a0fe6471a7742bb6640
Sebastian Delgado <sdalcega@gmail.com>
2018-01-26 19:39:47 -0500

    removed declaration

--- scripts/merge-pr.sh
+++ scripts/merge-pr.sh
@@ -53,7 +53,6 @@ get_pr_info() {
   if [[ $(echo "$pr_info" | jq -r .state) == closed ]]; then
     fatal "PR $pr is closed, will not merge"
   fi
-  local maintainer_can_modify=
   if [[ $(echo "$pr_info" | jq -r .maintainer_can_modify) == true ]] ||
      [[ $(echo "$pr_info" | jq -r .author_association) ==  MEMBER]] ||
      [[ $(echo "$pr_info" | jq -r .author_association) ==  OWNER]]; then
